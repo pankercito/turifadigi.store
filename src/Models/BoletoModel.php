35a79d8ca8ff9a2a78165643a272a0bdcc399914
ekiro <rodriguezeiker08@gmail.com>
2025-06-27 13:59:47 -0400

    fix: optimizar la consulta de obtención de boletos ordenando por número de boleto

--- src/Models/BoletoModel.php
+++ src/Models/BoletoModel.php
@@ -265,7 +265,7 @@ class BoletoModel
   {
     try {
       // Optimizamos la consulta para mejor rendimiento
-      $sql = "SELECT b.id_boleto, b.id_rifa, b.numero_boleto, b.estado AS estado, c.estado AS rifa_estado, r.id_rifa FROM boletos b INNER JOIN rifas r ON r.id_rifa = b.id_rifa INNER JOIN configuracion c ON c.id_configuracion = r.id_configuracion WHERE c.estado = 1 ORDER BY b.id_boleto ASC";
+      $sql = "SELECT b.id_boleto, b.id_rifa, b.numero_boleto, b.estado AS estado, c.estado AS rifa_estado, r.id_rifa FROM boletos b INNER JOIN rifas r ON r.id_rifa = b.id_rifa INNER JOIN configuracion c ON c.id_configuracion = r.id_configuracion WHERE c.estado = 1 ORDER BY b.numero_boleto ASC";
 
       $boletos = $this->db->consultar($sql, []);
 
